8349da16484456e167c33378e0a11fe78245391c
Aaron L <ayl@ualberta.ca>
2024-03-12 21:40:13 -0600

    Added the Shape class

--- src/game/tetris.js
+++ src/game/tetris.js
@@ -1,4 +1,54 @@
 /**
+ * Contiguous sets of blocks that will be dropping from the top of the screen
+ * @param {[[int, int]]} points An array of tuples that dictate the x and y position of each point making up the shape
+ */
+class Shape {
+    constructor(points) {
+        this.points = points
+    }
+
+    /**
+     * Freezes the shape in place by converting the points to static
+     * @param {[[int]]} board An array of arrays representing the Tetris board
+     */
+    freeze(board) {
+        for (let p = 0; p < this.points.length; p++) {
+            let rowNumber = this.points[p][0]
+            let columnNumber = this.points[p][1]
+            board[rowNumber][columnNumber] = 1
+        }
+    }
+
+    /**
+     * Lowers the shape by one row
+     * @param {[[int]]} board An array of arrays representing the Tetris board
+     * @return {bool} If the shape became static by hitting the ground
+     */
+    lowerShape(board) {
+        // Check if there's room beneath the shape and empty the current position
+        for (let p = 0; p < this.points.length; p++) {
+            let rowNumber = this.points[p][0]
+            let columnNumber = this.points[p][1]
+            if (rowNumber === 0 || board[rowNumber - 1][columnNumber] === 1) {
+                this.freeze(board)
+                return true
+            }
+            board[rowNumber][columnNumber] = 0
+        }
+
+        // Shift all of the points downwards and update the points array
+        for (let p = 0; p < this.points.length; p++) {
+            this.points[p][0]--;
+            let rowNumber = this.points[p][0]
+            let columnNumber = this.points[p][1]
+            board[rowNumber][columnNumber] = 2
+        }
+
+        return false
+    }
+}
+
+/**
  * Checks for a completed row in a Tetris board
  * @param {[[int]]} board An array of arrays representing the Tetris board
  * @return {int} The number of points to add to the player's score
@@ -97,6 +147,10 @@ function startTetris() {
     /*
         The board is a 13 x 25 grid that represents the state of the game. It will be filled with 1s (denoting a block) and 0s (denoting an empty space).
         (0, 0) denotes the element in the bottom left of the grid.
+        It has values as follows:
+            - 0: An empty space
+            - 1: A space filled with a static block
+            - 2: A space filled with part of a shape
     */
     var board = new Array(25)
     for (let i = 0; i < board.length; i++) {
@@ -110,7 +164,6 @@ function startTetris() {
         if (checkEndGame(board)) {
             break;
         }
-
 
         // Iterate every 100 ms
         sleep( 100 )
