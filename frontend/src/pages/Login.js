7700521c47125486ec56139d667bcb1b6128fe3d
AlphonsoD <alphonso@ualberta.ca>
2024-03-14 16:43:08 -0600

    refactored login and added basic input handling

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,24 @@
-import { Grid, Typography } from '@mui/material';
-import LoginForm from '../components/LoginForm';
+import { useState } from 'react';
+
+import { Grid, Typography, Box, TextField, Button } from '@mui/material';
 
 export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleLoginClick() {
+        console.log("username: " + username);
+        console.log("password: " + password);
+    }
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
+
     return (
         <Grid
             container
@@ -13,7 +30,39 @@ export default function Login() {
         >
             <Grid item xs={3}>
                 <Typography variant="h2" textAlign={"center"} sx={{ my: 2 }}>PentaParty</Typography>
-                <LoginForm />
+                <Box 
+                    width={400}
+                    height={400}
+                    display='flex'
+                    flexDirection='column'
+                    justifyContent='center'
+                    alignItems='center'
+                    sx={{ boxShadow: 2, borderRadius: '16px' }}
+                >
+                    <Typography variant="h4" sx={{ mb: 4 }}>Login</Typography>
+                    <TextField 
+                        label="Username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                        sx={{ width: '80%', mb: 2 }} />
+                    <TextField 
+                        label="Password" 
+                        type="password" 
+                        value={password}
+                        onChange={handlePasswordChange}
+                        sx={{ width: '80%', mb: 3 }} />
+                    <Button 
+                        variant="contained" 
+                        onClick={handleLoginClick}
+                        sx={{ mb: 1 }}>
+                            Login
+                    </Button>
+                    <Button 
+                        variant="outlined"
+                        href="/signup">
+                            Sign Up
+                    </Button>
+                </Box>
             </Grid>
         </Grid>
     )
