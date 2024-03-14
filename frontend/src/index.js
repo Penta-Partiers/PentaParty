53e93150f0824347f594a70ddf0870fc80d7686d
AlphonsoD <alphonso@ualberta.ca>
2024-03-14 16:43:16 -0600

    added login page routing link

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
+import Login from "./pages/Login"
 import SignUp from './pages/SignUp';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />
+  },
+  {
+    path: "/login",
+    element: <Login />
   },
   {
     path: "/signup",
