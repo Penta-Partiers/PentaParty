3e8d0c11388618b75354a548689535ffbb3298c7
AlphonsoD <alphonso@ualberta.ca>
2024-03-15 15:11:20 -0600

    basic game board and player view

--- frontend/src/components/GameBoard.js
+++ frontend/src/components/GameBoard.js
@@ -1,8 +1,22 @@
+const NUM_ROWS = 25;
+const NUM_COLS = 13;
+
 export default function GameBoard() {
-    const NUM_ROWS = 25;
-    const NUM_COLS = 13;
+    // Temporary - board initialization might be in a different file later
+    const board = new Array(NUM_ROWS)
+    for (let i = 0; i < board.length; i++) {
+        board[i] = new Array(NUM_COLS).fill(0)
+    }
 
     return (
-        <p>Game board component!</p>
+        <div className="border-2 border-solid border-slate-500 w-fit">
+            {board.map((row, rowIndex) => (
+                <div key={`${rowIndex}`} className="flex">
+                    {row.map((cell, colIndex) => (
+                        <div key={`${rowIndex}-${colIndex}`} className="w-[26px] aspect-square border border-solid border-slate-500 bg-black">t</div>
+                    ))}
+                </div>
+            ))}
+        </div>
     )
 }
--- frontend/src/pages/PlayerView.js
+++ frontend/src/pages/PlayerView.js
@@ -1,10 +1,26 @@
+import { Grid, Box } from '@mui/material';
+
 import GameBoard from '../components/GameBoard';
 
 export default function PlayerView() {
     return (
-        <div>
-            <p>Player view!</p>
-            <GameBoard />
+        <div className="min-h-screen flex justify-center items-center">
+            <Grid
+                container
+                spacing={1}
+                justifyContent="center"
+                alignItems="flex-start"
+            >
+                <Grid item>
+                    <GameBoard />
+                </Grid>
+                <Grid item>
+                    {/* Temporary, scoreboard will be a separate component later */}
+                    <Box width={200} height={200} className="border border-solid border-black">
+                        (scores go here eventually)
+                    </Box>
+                </Grid>
+            </Grid>
         </div>
     )
 }
