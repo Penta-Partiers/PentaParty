e628f2ee8ebd3349788fd2b66f8598265c1a51df
AlphonsoD <alphonso@ualberta.ca>
2024-03-16 20:19:04 -0600

    added testing values for player scoreboard

--- frontend/src/pages/PlayerView.js
+++ frontend/src/pages/PlayerView.js
@@ -1,13 +1,21 @@
-import { Grid, Box } from '@mui/material';
+import { Grid, Box, Typography } from '@mui/material';
 
 import GameBoard from '../components/GameBoard';
 
 export default function PlayerView() {
+    // Temporary variable for testing the scoreboard display
+    const playerScores = [
+        ["player_01", 1000],
+        ["player_02", 2000],
+        ["player_03", 3000],
+        ["player_04", 4000],
+    ];
+
     return (
         <div className="min-h-screen flex justify-center items-center">
             <Grid
                 container
-                spacing={1}
+                spacing={3}
                 justifyContent="center"
                 alignItems="flex-start"
             >
@@ -15,9 +23,23 @@ export default function PlayerView() {
                     <GameBoard />
                 </Grid>
                 <Grid item>
-                    {/* Temporary, scoreboard will be a separate component later */}
-                    <Box width={200} height={200} className="border border-solid border-black">
-                        (scores go here eventually)
+                    <Box 
+                        width={250} 
+                        height={250} 
+                        display="flex"
+                        flexDirection="column"
+                        justifyContent="center"
+                        alignItems="center"
+                        gap={1}
+                        className="border border-solid border-black"
+                    >
+                        <Typography variant="h4">Scoreboard</Typography>
+                        {playerScores.map((data, index) => (
+                            <div key={index} className="flex justify-between gap-3">
+                                <div><b>{data[0]}:</b></div>
+                                <div>{data[1]}</div>
+                            </div>
+                        ))}
                     </Box>
                 </Grid>
             </Grid>
