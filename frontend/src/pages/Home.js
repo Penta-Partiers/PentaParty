5a5d7ad58ea2a9b0ba3d53e5eda2c1765f1cb63d
AlphonsoD <alphonso@ualberta.ca>
2024-03-19 14:29:14 -0600

    removed console log line from home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,8 +4,6 @@ import { Context } from "../auth/AuthContext";
 export default function Home() {
     const {user} = useContext(Context);
 
-    console.log(user);
-
     return (
         <p>Home page! Hello {user.displayName ? user.displayName : user.email}!</p>
     )
