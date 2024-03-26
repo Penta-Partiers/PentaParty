b3694fa2f86aca71c29893293c334c2f6ec8bce0
AlphonsoD <alphonso@ualberta.ca>
2024-03-26 10:57:41 -0600

    more styling for game summary page

--- frontend/src/pages/GameSummary.js
+++ frontend/src/pages/GameSummary.js
@@ -1,18 +1,32 @@
+// Material UI
 import { Button, Typography } from "@mui/material"
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 
 export default function GameSummary() {
+    const results = [
+        {name: "player_01", score: 10000},
+        {name: "player_02", score: 1000},
+        {name: "player_03", score: 3000},
+        {name: "player_04", score: 2000},
+    ]
+
     return (
         <div className="min-h-screen flex justify-center items-center">
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center space-y-8 w-96">
                 <div className="flex flex-col items-center">
-                    <div>1st</div>
-                    <div>player_01</div>
-                    <div>10000</div>
+                    <Typography variant="h4">Winner</Typography>
+                    <div className="flex space-x-2 items-center">
+                        <EmojiEventsIcon sx={{ fontSize: 60 }}/>
+                        <Typography variant="h3"><b>player_01</b></Typography>
+                    </div>
                 </div>
-                <div>
-                    remaining places div
+                <div className="flex flex-col items-center w-full">
+                    <div className="flex justify-between items-center w-full">
+                        <Typography variant="h6"><b>Player</b></Typography>
+                        <Typography variant="h6"><b>Score</b></Typography>
+                    </div>
                 </div>
-                <div>Exit button</div>
+                <Button variant="outlined">Exit</Button>
             </div>
         </div>
     )
